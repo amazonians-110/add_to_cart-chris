34422e91ce62992410654ccf5ea4e00a83b3d0ec
Chris Tso <christophertso2017@u.northwestern.edu>
2019-02-08 19:20:35 -0800

    Refactored seeding script

---
+++ server/server.js
@@ -0,0 +1,47 @@
+// SERVER FILE
+const express = require('express');
+
+const app = express();
+const { getProduct } = require('../database/index.js');
+const { addToCart } = require('../database/index.js');
+const { itemsInCart } = require('../database/index.js');
+
+app.use(express.static('./client/dist'));
+
+const PORT = 3002;
+
+app.listen(PORT, console.log('listening at 3002'));
+
+app.get('/product/:id', (req, res) => {
+  const idToSearch = req.params.id;
+  console.log('idToSearch', idToSearch);
+  getProduct(idToSearch, (err, data) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(data);
+  });
+});
+
+app.put('/product/:id', (req, res) => {
+  // need to get the amount selected from the drop down menu from client
+  const idToSearch = req.params.id;
+  addToCart(idToSearch, (err, data) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(data);
+  });
+});
+
+app.get('/addtocart', (req, res) => {
+  itemsInCart((err, data) => {
+    if (err) {
+      res.status(400).send();
+      return;
+    }
+    res.status(200).send(data);
+  });
+});
