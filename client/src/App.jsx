863b9b00ea8cad1b7b40375c13332c29a627015a
Chris Tso <christophertso2017@u.northwestern.edu>
2019-02-11 14:19:20 -0800

    Created initial div with price and shipping info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import CSSModules from 'react-css-modules';
+import './App.css';
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return(
-            <div>HI</div>
-        )
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="price"> $19.99 </div>
+        <div className="freeshipping">
+           &
+          {' '}
+          <b>FREE Shipping</b>
+          {' '}
+          on orders over $25 shipped by Amazon.
+          {' '}
+          <span className="details">Details</span>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
