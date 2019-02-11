a5daee5d3ae1d92908741c4f3fe12e897a7c05ec
Chris Tso <christophertso2017@u.northwestern.edu>
2019-02-11 11:57:57 -0800

    Rendered React with server

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CSSModules from 'react-css-modules';
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>HI</div>
+        )
+    }
+}
+
+export default App;
