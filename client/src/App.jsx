e7e6fa5149c7f8a88a82bd242b642b09815c2dec
Chris Tso <christophertso2017@u.northwestern.edu>
2019-02-12 11:48:14 -0800

    Implemented closing of tooltip on outside click and text/style decorations of links on hover

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import CSSModules from 'react-css-modules';
 import './App.css';
+import DetailsBox from './DetailsBox.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+    
     };
+    this.child = React.createRef();
+    this.clickOutside = this.clickOutside.bind(this);
+  }
+
+  clickOutside(event) {
+    if (event.target.className === 'body' || event.target.className === 'container') {
+      this.child.current.setState({ clicked: false });
+    }
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="price"> $19.99 </div>
-        <div className="freeshipping">
-           &
-          {' '}
-          <b>FREE Shipping</b>
-          {' '}
-          on orders over $25 shipped by Amazon.
-          {' '}
-          <span className="details">Details</span>
+      <div className="body" onClick ={this.clickOutside}>
+        <div className="container">
+          <div className="price"> $19.99 </div>
+          <div className="freeshipping">
+            &
+            {' '}
+            <b>FREE Shipping</b>
+            {' '}
+            on orders over $25 shipped by Amazon.
+            {' '}
+            <DetailsBox ref={this.child} />
+          </div>
         </div>
       </div>
     );
---
+++ client/src/DetailsBox.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import './DetailsBox.css';
+
+class DetailsBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showMessage = this.showMessage.bind(this);
+    this.hideMessage = this.hideMessage.bind(this);
+    this.hideBorder = this.hideBorder.bind(this);
+    this.state = {
+      clicked: false,
+      button: true,
+    };
+  }
+
+  showMessage() {
+    const { clicked } = this.state;
+    this.setState({ clicked: true });
+    this.setState({ button: true });
+    if (clicked === true) {
+      this.setState({ button: false });
+    }
+  }
+
+  hideBorder() {
+    this.setState({ button: false });
+  }
+
+  hideMessage() {
+    this.setState({ clicked: false });
+  }
+
+  render() {
+    const { button } = this.state;
+    const { clicked } = this.state;
+    const style1 = {
+      border: button ? '1px solid orange' : '1px solid white',
+      boxShadow: button ? '0 0 3px #ff8000' : '',
+      float: 'right',
+    };
+    let message;
+    if (clicked) {
+      message = (
+        <div className="message" role="button" onClick={this.hideBorder}>
+          <p>
+            {' '}
+            <div className="shipping">
+              <b>Get free shipping</b>
+              <button type="button" style={style1} onClick={this.hideMessage}><b>X</b></button>
+            </div>
+            {' '}
+            <br />
+            <b>Free 5-8 business-day shipping</b>
+            {' '}
+            within the U.S. when you order $25 of eligible items sold or fulfilled by Amazon.
+            <br />
+            <div className="moreshipping">Or get 4-5 business-day shipping on this item for $5.99. (Prices may vary for AK and HI.)</div>
+            <br />
+            <a href="google.com">Learn more about free shipping</a>
+          </p>
+        </div>
+      );
+    }
+    return (
+      <div className="tooltip">
+        <span className="details" onClick={this.showMessage}>Details</span>
+        {message}
+      </div>
+    );
+  }
+}
+
+export default DetailsBox;
